6f7d03dd38d47b67bb52fe752438e9d67c0be5a7
Smruti Mahapatra <getsmruti03@gmail.com>
2025-05-15 01:14:50 +0530

    Added password encryption on signup

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    SALT_ROUNDS: process.env.SALT_ROUNDS
 }
 
