546913de7592f23dda5ccfc53fa1b5aa9d4909a2
Smruti Mahapatra <getsmruti03@gmail.com>
2025-05-18 11:31:13 +0530

    Added proxy using http-proxy-middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const rateLimit = require('express-rate-limit');
 const { ServerConfig, Logger } = require('./config');
 const apiRouter = require('./routes');
@@ -6,13 +7,15 @@ const apiRouter = require('./routes');
 const app = express();
 
 const limiter = rateLimit({
-	windowMs: 2 * 60 * 1000, // 2 minutes
-	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
+app.use('/flightService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: { '^/flightsService': '/' } }));
+app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true, pathRewrite: { '^/bookingService': '/' } }));
 app.use('/api', apiRouter);
 
 app.listen(ServerConfig.PORT, () => {
